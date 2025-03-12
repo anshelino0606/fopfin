bd37b70db299e5a79743d02d1ee13b19f64add2e
Nadiia Kurtian <nadiiakurtian@gmail.com>
2025-03-12 17:40:32 +0200

    Added seed data

--- backend/Infrastructure/Persistence/DatabaseInitializer.cs
+++ backend/Infrastructure/Persistence/DatabaseInitializer.cs
@@ -1,5 +1,3 @@
-using Microsoft.EntityFrameworkCore;
-
 namespace fopfin.Infrastructure.Persistence
 {
     public static class DatabaseInitializer
@@ -7,11 +5,29 @@ namespace fopfin.Infrastructure.Persistence
         public static void Initialize(AppDbContext context)
         {
             context.Database.Migrate();
+
             var sqlFilePath = "Migrations/schema.sql";
             if (File.Exists(sqlFilePath))
             {
                 var sql = File.ReadAllText(sqlFilePath);
                 context.Database.ExecuteSqlRaw(sql);
+            }
+
+            SeedData(context);
+        }
+
+        private static void SeedData(AppDbContext context)
+        {
+            if (!context.Users.Any())
+            {
+                context.Users.Add(new User(
+                    new Email("admin@example.com"),
+                    "hashedpassword",
+                    Role.Admin,
+                    TaxGroup.Individual
+                ));
+
+                context.SaveChanges();
             }
         }
     }
