7858557030b56fe140d4ae3a39f4eaf05e3b927c
anhelinamodenko <anhelinayolo04@icloud.com>
2025-03-13 21:02:35 +0200

    improv: add migration

---
+++ backend/Migrations/20250313190158_AddEntities.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    Icon = table.Column<string>(type: "text", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    UserId1 = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Categories_Users_UserId1",
+                        column: x => x.UserId1,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reports",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    UserId1 = table.Column<Guid>(type: "uuid", nullable: false),
+                    Month = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    TotalIncome = table.Column<decimal>(type: "numeric", nullable: false),
+                    TotalExpense = table.Column<decimal>(type: "numeric", nullable: false),
+                    GeneratedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    PdfUrl = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reports", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reports_Users_UserId1",
+                        column: x => x.UserId1,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TaxReports",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    UserId1 = table.Column<Guid>(type: "uuid", nullable: false),
+                    PeriodStart = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    PeriodEnd = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    TotalIncome = table.Column<decimal>(type: "numeric", nullable: false),
+                    TaxableIncome = table.Column<decimal>(type: "numeric", nullable: false),
+                    TaxDue = table.Column<decimal>(type: "numeric", nullable: false),
+                    PeriodType = table.Column<int>(type: "integer", nullable: false),
+                    GeneratedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    PdfUrl = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaxReports", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TaxReports_Users_UserId1",
+                        column: x => x.UserId1,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ObserverPermissions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    AdminId = table.Column<int>(type: "integer", nullable: false),
+                    AdminId1 = table.Column<Guid>(type: "uuid", nullable: false),
+                    ObserverId = table.Column<int>(type: "integer", nullable: false),
+                    ObserverId1 = table.Column<Guid>(type: "uuid", nullable: false),
+                    CategoryId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ObserverPermissions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ObserverPermissions_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ObserverPermissions_Users_AdminId1",
+                        column: x => x.AdminId1,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ObserverPermissions_Users_ObserverId1",
+                        column: x => x.ObserverId1,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_UserId1",
+                table: "Categories",
+                column: "UserId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ObserverPermissions_AdminId1",
+                table: "ObserverPermissions",
+                column: "AdminId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ObserverPermissions_CategoryId",
+                table: "ObserverPermissions",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ObserverPermissions_ObserverId1",
+                table: "ObserverPermissions",
+                column: "ObserverId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reports_UserId1",
+                table: "Reports",
+                column: "UserId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaxReports_UserId1",
+                table: "TaxReports",
+                column: "UserId1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ObserverPermissions");
+
+            migrationBuilder.DropTable(
+                name: "Reports");
+
+            migrationBuilder.DropTable(
+                name: "TaxReports");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
