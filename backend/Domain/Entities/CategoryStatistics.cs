c6a3f71bbebea17e59058ee947d996fa04841bb3
Nadiia Kurtian <nadiiakurtian@gmail.com>
2025-04-10 19:49:52 +0300

    fixed error

--- backend/Domain/Entities/CategoryStatistics.cs
+++ backend/Domain/Entities/CategoryStatistics.cs
@@ -1,14 +1,13 @@
-public class CategoryStatistics
+namespace fopfin.Domain.Entities
 {
-    public int Id { get; set; }
-    
-    public int ReportId { get; set; }
-    public Report Report { get; set; }
-    
-    public int? CategoryId { get; set; }
-    public Category Category { get; set; }
-    
-    public decimal Total { get; set; }
-    
-    public string Type { get; set; } // Expense or Income
+    public class CategoryStatistics
+    {
+        public int Id { get; set; }
+        public int ReportId { get; set; }
+        public Report Report { get; set; }
+        public int? CategoryId { get; set; }
+        public Category Category { get; set; }
+        public decimal Total { get; set; }
+        public string Type { get; set; } // Expense or Income
+    }
 }
